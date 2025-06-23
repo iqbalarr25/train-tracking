203fdeb6e837c6434ccbcc85d9ff137fdf2f708e
iqbal <iqbal.arrafi@transtrack.id>
2025-06-23 13:09:33 +0700

    :sparkles: add relation to train class

--- internal/features/model/train.go
+++ internal/features/model/train.go
@@ -4,8 +4,11 @@ import "github.com/google/uuid"
 
 type (
 	Train struct {
-		ID   uuid.UUID `gorm:"primaryKey"`
-		Name string    `gorm:"varchar(255)"`
+		ID           uuid.UUID `gorm:"primaryKey"`
+		Name         string    `gorm:"varchar(255)"`
+		TrainClassID uuid.UUID
+
+		TrainClass TrainClass `gorm:"foreignKey:TrainClassID;references:ID"`
 	}
 
 	TrainClass struct {
