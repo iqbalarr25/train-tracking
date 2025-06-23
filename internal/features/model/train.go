4f9c526601c64963ea3ec272d492bd1360d204ed
iqbal <iqbal.arrafi@transtrack.id>
2025-06-23 11:54:03 +0700

    :sparkles: migration train

--- internal/features/model/train.go
+++ internal/features/model/train.go
@@ -3,6 +3,11 @@ package model
 import "github.com/google/uuid"
 
 type (
+	Train struct {
+		ID   uuid.UUID `gorm:"primaryKey"`
+		Name string    `gorm:"varchar(255)"`
+	}
+
 	TrainClass struct {
 		ID   uuid.UUID `gorm:"primaryKey"`
 		Name string    `gorm:"varchar(255)"`
